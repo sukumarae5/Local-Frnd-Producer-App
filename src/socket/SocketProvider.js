0531f7607f5f7e45be7d380ab0672eb1b9aaa3f1
vijaya-durga-12 <arepallivijayadurga12@gmail.com>
2026-04-01 16:58:14 +0530

    commited the small changes

--- src/socket/SocketProvider.js
+++ src/socket/SocketProvider.js
@@ -3,12 +3,12 @@ import { AppState } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createSocket, destroySocket } from './globalSocket';
 import { useDispatch, useStore } from 'react-redux';
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
 import {
   incomingCallAccept,
   incomingCallReject,
-  incomingCallRinging
-} from "../features/calls/callAction";
+  incomingCallRinging,
+} from '../features/calls/callAction';
 
 import { fetchUnreadCount } from '../features/notification/notificationAction';
 import {
@@ -21,209 +21,238 @@ import { CHAT_MARK_READ_SUCCESS } from '../features/chat/chatType';
 export const SocketContext = createContext(null);
 
 const SocketProvider = ({ children }) => {
-
   const dispatch = useDispatch();
   const store = useStore();
-const token = useSelector(state => state.auth?.token);
-console.log("SocketProvider token:", token);
+  const token = useSelector(state => state.auth?.token);
+  console.log('SocketProvider token:', token);
   const socketRef = useRef(null);
   const appState = useRef(AppState.currentState);
 
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
-
     let socket;
 
     const init = async () => {
-
       // const token = await AsyncStorage.getItem('twittoke');
 
-       if (!token) {
-    console.log("❌ No token, socket not connecting");
-    destroySocket();
-    setConnected(false);
-    return;
-  }
+      if (!token) {
+        console.log('❌ No token, socket not connecting');
+        destroySocket();
+        setConnected(false);
+        return;
+      }
 
-       console.log("🔑 SOCKET TOKEN:", token);
+      console.log('🔑 SOCKET TOKEN:', token);
 
-  socket = createSocket(token);
-  socketRef.current = socket;
+      socket = createSocket(token);
+      socketRef.current = socket;
 
-  socket.on('connect', () => {
-    console.log("✅ SOCKET CONNECTED");
-    setConnected(true);
-  });
+      socket.on('connect', () => {
+        console.log('✅ SOCKET CONNECTED');
+        setConnected(true);
+      });
 
-  socket.on('disconnect', () => {
-    console.log("❌ SOCKET DISCONNECTED");
-    setConnected(false);
-  });
+      socket.on('disconnect', () => {
+        console.log('❌ SOCKET DISCONNECTED');
+        setConnected(false);
+      });
       /* NOTIFICATIONS */
-      socket.on("new_notification", () => {
+      socket.on('new_notification', () => {
         dispatch(fetchUnreadCount());
       });
 
-      socket.on("new_message_notification", data => {
-  console.log("🔔 New message notification:", data);
+      socket.on('new_message_notification', data => {
+        console.log('🔔 New message notification:', data);
 
-  // optional:
-  dispatch(fetchUnreadCount());
-});
+        // optional:
+        dispatch(fetchUnreadCount());
+      });
 
-// socket.on('chat_receive', msg => {
+      // socket.on('chat_receive', msg => {
 
-//   const state = store.getState();
-//   const myId = state.user.userdata?.user?.user_id;
+      //   const state = store.getState();
+      //   const myId = state.user.userdata?.user?.user_id;
 
-//   if (!myId) return;
+      //   if (!myId) return;
 
-//   const senderId = msg.sender_id ?? msg.senderId;
-//   const receiverId = msg.receiver_id ?? msg.receiverId;
-// const isSender = Number(senderId) === Number(myId);
+      //   const senderId = msg.sender_id ?? msg.senderId;
+      //   const receiverId = msg.receiver_id ?? msg.receiverId;
+      // const isSender = Number(senderId) === Number(myId);
 
-//   if (senderId !== myId && receiverId !== myId) return;
+      //   if (senderId !== myId && receiverId !== myId) return;
 
-//   const otherUserId =
-//     Number(senderId) === Number(myId)
-//       ? receiverId
-//       : senderId;
+      //   const otherUserId =
+      //     Number(senderId) === Number(myId)
+      //       ? receiverId
+      //       : senderId;
 
-// const normalizedMsg = {
-//   ...(msg.message ?? msg),
-//   is_read: msg.is_read ?? 0,
-//   delivered: msg.delivered ?? 0, // ✅ ADD THIS
-// };
+      // const normalizedMsg = {
+      //   ...(msg.message ?? msg),
+      //   is_read: msg.is_read ?? 0,
+      //   delivered: msg.delivered ?? 0, // ✅ ADD THIS
+      // };
 
-//   dispatch(chatMessageAdd({
-//     otherUserId,
-//     message: normalizedMsg,
-//   }));
+      //   dispatch(chatMessageAdd({
+      //     otherUserId,
+      //     message: normalizedMsg,
+      //   }));
 
-//   const activeUser = state.chat.activeUser;
-// console.log("📨 Received message from", otherUserId, "active chat is", activeUser) ;
-// console.log("Message details:", normalizedMsg) ;
-// console.log("My ID:", myId) ;
-// console.log("Sender ID:", senderId) ;
-// console.log("Receiver ID:", receiverId) ;
-// console.log("Is sender:", isSender) ;
-//   if (
-//   Number(activeUser) === Number(otherUserId) &&
-//   Number(senderId) !== Number(myId)
-// ) {
-//   socket.emit('chat_read', {
-//     messageId: normalizedMsg.message_id,
-//   });
-// }
-//   // ✅ STEP 3: IF NOT OPEN → INCREASE UNREAD
-//   if (Number(receiverId) === Number(myId)) {
-//     dispatch(chatUnreadIncrease(senderId));
-//   }
-// });
-      
+      //   const activeUser = state.chat.activeUser;
+      // console.log("📨 Received message from", otherUserId, "active chat is", activeUser) ;
+      // console.log("Message details:", normalizedMsg) ;
+      // console.log("My ID:", myId) ;
+      // console.log("Sender ID:", senderId) ;
+      // console.log("Receiver ID:", receiverId) ;
+      // console.log("Is sender:", isSender) ;
+      //   if (
+      //   Number(activeUser) === Number(otherUserId) &&
+      //   Number(senderId) !== Number(myId)
+      // ) {
+      //   socket.emit('chat_read', {
+      //     messageId: normalizedMsg.message_id,
+      //   });
+      // }
+      //   // ✅ STEP 3: IF NOT OPEN → INCREASE UNREAD
+      //   if (Number(receiverId) === Number(myId)) {
+      //     dispatch(chatUnreadIncrease(senderId));
+      //   }
+      // });
 
+      socket.on('chat_receive', msg => {
+        const state = store.getState();
+        const myId = state.user.userdata?.user?.user_id;
 
-socket.on('chat_receive', msg => {
+        const senderId = msg.sender_id ?? msg.senderId;
+        const receiverId = msg.receiver_id ?? msg.receiverId;
 
-  const state = store.getState();
-  const myId = state.user.userdata?.user?.user_id;
+        const otherUserId =
+          Number(senderId) === Number(myId) ? receiverId : senderId;
 
-  const senderId = msg.sender_id ?? msg.senderId;
-  const receiverId = msg.receiver_id ?? msg.receiverId;
+        const normalizedMsg = {
+          ...(msg.message ?? msg),
+          is_read: msg.is_read ?? 0,
+          delivered: msg.delivered ?? 0,
+        };
 
-  const otherUserId =
-    Number(senderId) === Number(myId)
-      ? receiverId
-      : senderId;
-
-  const normalizedMsg = {
-    ...(msg.message ?? msg),
-    is_read: msg.is_read ?? 0,
-    delivered: msg.delivered ?? 0,
-  };
-
-  // ✅ DO NOT BLOCK SENDER
-  dispatch(chatMessageAdd({
-    otherUserId,
-    message: normalizedMsg,
-  }));
-
-});
+        // ✅ DO NOT BLOCK SENDER
+        dispatch(
+          chatMessageAdd({
+            otherUserId,
+            message: normalizedMsg,
+          }),
+        );
+      });
       socket.on('chat_read_update', ({ messageId }) => {
         dispatch({
           type: CHAT_MARK_READ_SUCCESS,
           payload: { messageId },
         });
       });
-socket.on('chat_delivered', ({ messageId }) => {
-  dispatch({
-    type: "CHAT_MESSAGE_DELIVERED",
-    payload: { messageId }
-  });
-});
+      socket.on('chat_delivered', ({ messageId }) => {
+        dispatch({
+          type: 'CHAT_MESSAGE_DELIVERED',
+          payload: { messageId },
+        });
+      });
 
+      // socket.on('chat_read_all_update', ({ conversationId }) => {
+      //   const state = store.getState();
 
-     socket.on("incoming_call", (data) => {
+      //   const conversations = state.chat.conversations;
 
-  const isFriend = data.call_mode === "FRIEND";
+      //   const updated = {};
 
-  // ✅ FRIEND → SHOW UI
-  if (isFriend) {
-    dispatch(incomingCallRinging({
-      session_id: data.session_id,
-      call_type: data.call_type,
-      direction: "INCOMING",
-      from_user: data.from,
-      is_friend: true,
-      status: "RINGING",
-      call_mode: "FRIEND",
-    }));
-    return;
-  }
+      //   for (const uid in conversations) {
+      //     updated[uid] = conversations[uid].map(m => {
+      //       const msg = m.message ?? m;
 
-  // RANDOM / DIRECT → AUTO ACCEPT
-socket.emit("call_accept", {
-  session_id: data.session_id,
-});
+      //       return {
+      //         ...msg,
+      //         is_read: 1,
+      //       };
+      //     });
+      //   }
 
-// 🔥 IMPORTANT: update redux immediately
-dispatch(incomingCallAccept({
-  session_id: data.session_id,
-  call_type: data.call_type,
-  status: "ACCEPTED",
-  is_friend: false,
-  direction: "INCOMING",
-  call_mode: data.call_mode || "RANDOM",
-}));
+      //   dispatch({
+      //     type: 'CHAT_MARK_READ_SUCCESS',
+      //     payload: {
+      //       otherUserId: state.chat.activeUser,
+      //     },
+      //   });
+      // });
 
-});
+      socket.on('chat_read_all_update', ({ otherUserId }) => {
+        dispatch({
+          type: 'CHAT_MARK_READ_SUCCESS',
+          payload: {
+            otherUserId, // ✅ use correct user
+          },
+        });
+      });
 
-// socket.on("call_accepted", (data) => {
-//   console.log("CALL ACCEPTED EVENT:", data);
-//   dispatch(incomingCallAccept(data));
-// });
+      socket.on('incoming_call', data => {
+        const isFriend = data.call_mode === 'FRIEND';
 
-socket.on("call_accepted", (data) => {
+        // ✅ FRIEND → SHOW UI
+        if (isFriend) {
+          dispatch(
+            incomingCallRinging({
+              session_id: data.session_id,
+              call_type: data.call_type,
+              direction: 'INCOMING',
+              from_user: data.from,
+              is_friend: true,
+              status: 'RINGING',
+              call_mode: 'FRIEND',
+            }),
+          );
+          return;
+        }
 
-  dispatch(incomingCallAccept({
-    session_id: data.session_id,
-    call_type: data.call_type,
-    status: "ACCEPTED",
-    is_friend: data.is_friend || false,
-  }));
+        // RANDOM / DIRECT → AUTO ACCEPT
+        socket.emit('call_accept', {
+          session_id: data.session_id,
+        });
 
-});
-socket.on("call_rejected", (data) => {
+        // 🔥 IMPORTANT: update redux immediately
+        dispatch(
+          incomingCallAccept({
+            session_id: data.session_id,
+            call_type: data.call_type,
+            status: 'ACCEPTED',
+            is_friend: false,
+            direction: 'INCOMING',
+            call_mode: data.call_mode || 'RANDOM',
+          }),
+        );
+      });
 
-  console.log("❌ CALL REJECTED:", data);
+      // socket.on("call_accepted", (data) => {
+      //   console.log("CALL ACCEPTED EVENT:", data);
+      //   dispatch(incomingCallAccept(data));
+      // });
 
-  dispatch(incomingCallReject({
-    ...data,
-    status: "REJECTED",
-  }));
+      socket.on('call_accepted', data => {
+        dispatch(
+          incomingCallAccept({
+            session_id: data.session_id,
+            call_type: data.call_type,
+            status: 'ACCEPTED',
+            is_friend: data.is_friend || false,
+          }),
+        );
+      });
+      socket.on('call_rejected', data => {
+        console.log('❌ CALL REJECTED:', data);
 
-});
+        dispatch(
+          incomingCallReject({
+            ...data,
+            status: 'REJECTED',
+          }),
+        );
+      });
     };
 
     init();
@@ -234,18 +263,13 @@ socket.on("call_rejected", (data) => {
       }
       destroySocket();
     };
-
   }, [token]);
 
   /* APP STATE RECONNECT */
 
   useEffect(() => {
     const sub = AppState.addEventListener('change', next => {
-
-      if (
-        appState.current.match(/inactive|background/) &&
-        next === 'active'
-      ) {
+      if (appState.current.match(/inactive|background/) && next === 'active') {
         if (socketRef.current && !socketRef.current.connected) {
           socketRef.current.connect();
         }
